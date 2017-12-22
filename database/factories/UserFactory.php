743df1957d12a0d8b4d2b202ff95ea9017d0087e
nguyenhathanh2212 <thanhtdk2212@gmail.com>
2017-12-22 11:09:37 +0700

    seeder

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -23,3 +23,8 @@ $factory->define(App\User::class, function (Faker $faker) {
         'remember_token' => str_random(10),
     ];
 });
+$factory->define(App\Task::class, function (Faker $faker) {
+    return [
+        'name' => $faker->name,
+    ];
+});
